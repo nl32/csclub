5c8e93172baa9d5235f27b8e6fea7ed94db87799
Ethan Bickel <notethanbickel@gmail.com>
2022-11-29 13:38:23 -0600

    added about page

---
+++ app/routes/about.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <>
+      <div className="pl-2 flex flex-col bg-blue-500 h-screen w-screen justify-center items-center content-center text-center"></div>
+    </>
+  );
+}
