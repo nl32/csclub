debdbcc7e9453965b3f36307f26a6c1f32709334
Ethan Bickel <notethanbickel@gmail.com>
2022-12-03 13:33:26 -0600

    Added styling to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,7 +1,31 @@
+import { Link } from "@remix-run/react";
+
 export default function About() {
   return (
     <>
-      <div className="pl-2 flex flex-col bg-blue-500 h-screen w-screen justify-center items-center content-center text-center"></div>
+      <div className="bg-blue-800 h-screen w-screen">
+        <div className=" absolute top-2 left-2 hover:scale-125 transition-all">
+          <Link to={"/"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="48"
+              height="48"
+              className="fill-yellow-400 hover:fill-yellow-500"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 9V8l-4 4 4 4v-3h4v-2h-4z" />
+            </svg>
+          </Link>
+        </div>
+        <div className="ml-40 pt-20 flex flex-col justify-center text-left">
+          <div className="justify-self-start my-2">
+            <h1 className="text-transparent bg-clip-text text-8xl font-extrabold bg-gradient-to-br from-yellow-300 to-red-300">
+              About us
+            </h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
